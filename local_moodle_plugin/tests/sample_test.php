8168126fc33e9e249df919ffaeeb6d448bc59091
Art4 <art4@wlabs.de>
2023-11-02 16:28:08 +0100

    Add phpdoc for test class

--- local_moodle_plugin/tests/sample_test.php
+++ local_moodle_plugin/tests/sample_test.php
@@ -1,6 +1,9 @@
 <?php
-namespace locale_moodle_plugin;
+namespace local_moodle_plugin;
 
+/**
+ * @package local_moodle_plugin
+ */
 class sample_test extends \basic_testcase {
     public function test_equals(): void {
         $a = 1 + 2;
