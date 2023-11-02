45152522dcd93f86cd4e3efabb8f4e2868410c3e
Art4 <art4@wlabs.de>
2023-11-02 09:49:03 +0100

    Create scripts for lint, phpcpd, phpmd and phpcs

--- local_moodle_plugin/version.php
+++ local_moodle_plugin/version.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Version metadata for the local_moodle_plugin plugin.
  *
@@ -21,6 +36,6 @@ $plugin->supported = [
     403,
 ];
 $plugin->incompatible = 404; // Available as of Moodle 3.9.0 or later.
-$plugin->maturity = MATURITY_ALPHA; // one of: MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
+$plugin->maturity = MATURITY_ALPHA; // One of: MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE.
 
 $plugin->dependencies = [];
