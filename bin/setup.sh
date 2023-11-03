c210109a1025021c99be0da7c9eec94d17283db7
Art4 <art4@wlabs.de>
2023-11-03 11:57:05 +0100

    improve setup.sh, ignore moodle folders, update README.md

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-composer create-project moodlehq/moodle-plugin-ci ./moodle-plugin-ci ^4
+PROJECT_FOLDER="$( cd "$(dirname "$0")/.." ; pwd -P )"
 
 # Install moodle-plugin-ci
-moodle-plugin-ci/bin/moodle-plugin-ci install --plugin ./local_moodle_plugin --db-host=db --branch=MOODLE_402_STABLE --db-type=mariadb --moodle=moodle
+if [ ! -d $PROJECT_FOLDER/moodle-plugin-ci ]; then
+    composer create-project moodlehq/moodle-plugin-ci $PROJECT_FOLDER/moodle-plugin-ci ^4
+fi
+
+# Remove previous moodle folder
+if [ -d $PROJECT_FOLDER/moodle-plugin-ci ]; then
+    echo "Deleting moodle folder"
+    # rm -r $PROJECT_FOLDER/moodle
+    mv $PROJECT_FOLDER/moodle $PROJECT_FOLDER/moodle.deleteme
+fi
+
+# Install moodle
+moodle-plugin-ci/bin/moodle-plugin-ci install --plugin $PROJECT_FOLDER/local_moodle_plugin --db-host=db --branch=MOODLE_402_STABLE --db-type=mariadb --moodle=moodle
